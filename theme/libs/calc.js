e34346460b64bd246d8b6085e79e2610c328dcd0
Guest007 <trash4@yandex.ru>
2014-04-08 16:52:30 +0400

    counters and calc fix

--- theme/libs/calc.js
+++ theme/libs/calc.js
@@ -48,7 +48,7 @@
                     checkbox = $(this).find('input[type="checkbox"]'),
                     color_ = $(this).find('span.colors').data('price').replace(',', '.'),
                     materials_ = $(this).find('span.materials').data('price').replace(',', '.');
-                hidden.not('.ratioб .colors, .materials').val(0);
+                hidden.not('.ratio, .colors, .materials').val(0);
                 $this.find('input[name="count"]').val('');
                 $this.find('input[name="count_hidden"]').val(0);
 
